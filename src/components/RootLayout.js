0cfb3f97c500d065d9b801d95420611a4ec953eb
harshal <harshalc562@gmail.com>
2023-09-09 01:11:15 +0530

    done routing between cart and dashboard

--- src/components/RootLayout.js
+++ src/components/RootLayout.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Navbar from './Navbar'
 
 const RootLayout = () => {
   return (
     <>
-      <h1>Navigation</h1>
+      <Navbar />
       <main>
         <Outlet />
       </main>
