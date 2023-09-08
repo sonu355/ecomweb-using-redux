b7e1610390f49e8f4537aa2839c021dd89966ac0
harshal <harshalc562@gmail.com>
2023-09-09 00:50:47 +0530

    now doing some navbar thing

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Product from './components/Product';
 import Dashboard from './components/Dashboard';
 import Cart from './components/Cart';
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom';
+import RootLayout from './components/RootLayout';
 
 function App() {
-  const route = createBrowserRouter(createRoutesFromElements(
-      <Route>
+  const router = createBrowserRouter(createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
         <Route index element={<Dashboard />}></Route>
         <Route path="/cart" element={<Cart />}></Route>
       </Route>
@@ -15,7 +16,7 @@ function App() {
 
   return (
     <div className="App">
-      <Product />
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
