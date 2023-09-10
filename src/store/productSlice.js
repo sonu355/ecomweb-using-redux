40121282b39f3c08a70384f818765e454f0924f5
harshal <harshalc562@gmail.com>
2023-09-10 13:34:51 +0530

    handling loading of the data

--- src/store/productSlice.js
+++ src/store/productSlice.js
@@ -2,6 +2,7 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 const initialState = {
   data: [],
+  status: "idle"
 };
 
 const productSlice = createSlice({
@@ -11,11 +12,20 @@ const productSlice = createSlice({
     // fetchProducts(state, action) {
     //   state.data = action.payload;
     // },
-
   },
   extraReducers: (builder) => {
-     
-  }
+    builder
+    .addCase(getProducts.pending, (state, action) => {
+       state.status = "loading";
+    })
+    .addCase(getProducts.fulfilled, (state, action) => {
+      state.data = action.payload;
+      state.status = "idle";
+   })
+    .addCase(getProducts.rejected, (state, action) => {
+      state.status = "error"
+    });
+  },
 });
 
 export const { fetchProducts } = productSlice.actions;
@@ -28,19 +38,24 @@ export default productSlice.reducer;
 //     dispatch(fetchProducts(result));
 //   };
 // }
-export function getProducts() {
-  return async function getProductsThunk(dispatch, getState) {
-    try {
-      const response = await fetch("https://fakestoreapi.com/products");
-      if (!response.ok) {
-        throw new Error("Failed to fetch products");
-      }
+// export function getProducts() {
+//   return async function getProductsThunk(dispatch, getState) {
+//     try {
+//       const response = await fetch("https://fakestoreapi.com/products");
+//       if (!response.ok) {
+//         throw new Error("Failed to fetch products");
+//       }
 
-      const result = await response.json();
-      dispatch(fetchProducts(result));
-    } catch (error) {
-      console.error("Error fetching products:", error);
-      // Handle the error or dispatch an action to indicate the error.
-    }
-  };
-}
+//       const result = await response.json();
+//       dispatch(fetchProducts(result));
+//     } catch (error) {
+//       console.error("Error fetching products:", error);
+//       // Handle the error or dispatch an action to indicate the error.
+//     }
+//   };
+// }
+export const getProducts = createAsyncThunk("products/get", async () => {
+  const data = await fetch("https://fakestoreapi.com/products");
+  const result = data.json();
+  return result;
+});
